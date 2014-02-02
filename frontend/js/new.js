dc14bb0a8db523fc19121da64ed0a5f5a9288884
Tom Wiesing <tkw01536@gmail.com>
2014-02-02 02:09:36 +0100

    Added more frontend stuffs

---
+++ frontend/js/new.js
@@ -0,0 +1,8 @@
+$(function(){
+	var id = location.hash.substring(1);
+
+	$(document.getElementById("id")).val(id); 
+
+	$(document.getElementById("go_fail")).val(location.pathname + location.hash); 
+	$(document.getElementById("go_success")).val("/courses/" + location.hash); 
+}); 
